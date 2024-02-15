e9527eed6c9a7ede87b48d1bdc1b7423c3b9e8fc
NielsTRS <trsniels@gmail.com>
2024-02-15 11:28:25 +0100

    ajout redirection

--- src/shell.c
+++ src/shell.c
@@ -8,14 +8,36 @@
 #include "csapp.h"
 
 void execcmd(struct cmdline *l) {
-    int i;
-    for (i = 0; l->seq[i] != 0; i++) {
+
+    for (int i = 0; l->seq[i] != 0; i++) {
         char **cmd = l->seq[i];
         if (strcmp(cmd[0], "quit") == 0) {
             exit(0);
         } else {
             int pid = Fork();
-            if (pid == 0) { // fils 
+            if (pid == -1) {
+                perror("Erreur lors de la création du processus fils");
+                exit(1);
+            } else if (pid == 0) { // fils
+                if (l->in != NULL) {
+                    int fd_in = open(l->in, O_RDONLY);
+                    if (fd_in == -1) {
+                        perror("Erreur lors de l'ouverture du fichier d'entrée");
+                        exit(1);
+                    }
+                    dup2(fd_in, 0);
+                    close(fd_in);
+                }
+                if (l->out != NULL) {
+                    int fd_out = open(l->out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    if (fd_out == -1) {
+                        perror("Erreur lors de l'ouverture du fichier de sortie");
+                        exit(1);
+                    }
+                    dup2(fd_out, 1);
+                    close(fd_out);
+                }
+
                 execvp(cmd[0], cmd);
                 exit(0);
             } else { // père
@@ -24,7 +46,6 @@ void execcmd(struct cmdline *l) {
         }
     }
 }
-
 
 int main() {
     while (1) {
