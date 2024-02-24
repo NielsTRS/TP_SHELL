a8c96941215d796d638bb9f2b4039aa9f10eddec
NielsTRS <trsniels@gmail.com>
2024-02-24 23:14:22 +0100

    Ajout commentaires

--- src/shell.c
+++ src/shell.c
@@ -4,10 +4,12 @@
 #include "csapp.h"
 #include <pthread.h>
 
+// constante sur l'état des processus
 #define FINISHED_STATE 0
 #define RUNNING_STATE 1
 #define SUSPENDED_STATE -1
 
+// Structure pour les processus
 typedef struct {
     pid_t pid;
     int state;
@@ -18,17 +20,18 @@ typedef struct {
 typedef struct {
     Process *processes;
     int nb;
-    pthread_mutex_t mutex;
+    pthread_mutex_t mutex; // gestionnaire des sections critiques
 } ProcessesList;
 
-ProcessesList *bg_processes;
-ProcessesList *fg_processes;
+ProcessesList *bg_processes; // arrière plan
+ProcessesList *fg_processes; // premier plan
 
+// handler du SIGCHLD
 void handler_chld(int sig) {
     pid_t pid;
     int status;
     while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED)) > 0) {
-        pthread_mutex_lock(&bg_processes->mutex);
+        pthread_mutex_lock(&bg_processes->mutex); // section critique
         // mises à jours
         for (int i = 0; i < bg_processes->nb; i++) {
             if (bg_processes->processes[i].pid == pid) {
@@ -39,7 +42,7 @@ void handler_chld(int sig) {
                 }
             }
         }
-        pthread_mutex_unlock(&bg_processes->mutex);
+        pthread_mutex_unlock(&bg_processes->mutex); // fin de section critique
 
         pthread_mutex_lock(&fg_processes->mutex);
         // mises à jours
@@ -56,7 +59,7 @@ void handler_chld(int sig) {
     }
 }
 
-
+// handler du SIGINT
 void handler_stop(int sig) {
     pthread_mutex_lock(&fg_processes->mutex);
     for (int i = 0; i < fg_processes->nb; i++) {
@@ -67,7 +70,7 @@ void handler_stop(int sig) {
     printf("\n");
 }
 
-
+// handle du SIGTSTP
 void handler_suspend(int sig) {
     pthread_mutex_lock(&fg_processes->mutex);
     for (int i = 0; i < fg_processes->nb; i++) {
@@ -78,9 +81,10 @@ void handler_suspend(int sig) {
     printf("\n");
 }
 
+// fonction qui traite les commandes intégrées au shell
 int exec_shell_cmd(struct cmdline *l) {
     char **cmd = l->seq[0];
-    if (strcmp(cmd[0], "quit") == 0) { // commande intégrée au shell
+    if (strcmp(cmd[0], "quit") == 0) {
         exit(EXIT_SUCCESS);
     }
     if (strcmp(cmd[0], "jobs") == 0) {
@@ -185,6 +189,7 @@ int exec_shell_cmd(struct cmdline *l) {
     return 0;
 }
 
+// donne la taille de la commande
 int cmd_size(char **cmd) {
     int nb = 0;
     while (cmd[nb] != NULL) {
@@ -193,6 +198,7 @@ int cmd_size(char **cmd) {
     return nb;
 }
 
+// compte le nombre de commande
 int count_cmd(struct cmdline *l) {
     int nb = 0;
     while (l->seq[nb]) {
@@ -201,6 +207,7 @@ int count_cmd(struct cmdline *l) {
     return nb;
 }
 
+// redirection entrante
 void redirect_in(struct cmdline *l) {
     if (l->in != NULL) {
         int fd_in = Open(l->in, O_RDONLY, 0644);
@@ -213,6 +220,7 @@ void redirect_in(struct cmdline *l) {
     }
 }
 
+// redirection sortante
 void redirect_out(struct cmdline *l) {
     if (l->out != NULL) {
         int fd_out = Open(l->out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -225,8 +233,11 @@ void redirect_out(struct cmdline *l) {
     }
 }
 
+// fonction qui traite les commandes qui ne sont pas intégrées au shell
 void exec_cmd(struct cmdline *l) {
     int nb = count_cmd(l);
+
+    // allocation mémoire
     pid_t *pids = malloc(sizeof(pid_t) * nb);
     int **pipes = malloc(sizeof(int) * nb - 1);
 
@@ -273,12 +284,14 @@ void exec_cmd(struct cmdline *l) {
             }
 
             char **cmd = l->seq[i];
+            // execution commande
             if (execvp(cmd[0], cmd) == -1) {
                 perror(cmd[0]);
                 exit(EXIT_FAILURE);
             }
         } else { // père
             if (l->bg) {
+                // Sauvegarde dans la structure des processus en arrière plan
                 pthread_mutex_lock(&bg_processes->mutex);
                 printf("[%d] %d\n", bg_processes->nb + 1, pids[i]);
 
@@ -301,6 +314,7 @@ void exec_cmd(struct cmdline *l) {
                 bg_processes->nb++;
                 pthread_mutex_unlock(&bg_processes->mutex);
             } else {
+                // sauvegarde dans la structure des processus au premier plan
                 pthread_mutex_lock(&fg_processes->mutex);
 
                 char **cmd = l->seq[i];
@@ -327,11 +341,13 @@ void exec_cmd(struct cmdline *l) {
 
     // père
 
+    // fermeture des pipes
     for (int i = 0; i < nb - 1; i++) {
         Close(pipes[i][0]);
         Close(pipes[i][1]);
     }
 
+    // Problème : partie bloquante lorsqu'on fait un Ctrl+Z
     if (!l->bg) {
         for (int i = 0; i < fg_processes->nb; i++) {
             if (fg_processes->processes[i].state != SUSPENDED_STATE) {
@@ -349,10 +365,12 @@ void exec_cmd(struct cmdline *l) {
 }
 
 int main() {
+    // les signaux
     Signal(SIGCHLD, handler_chld);
     Signal(SIGINT, handler_stop);
     Signal(SIGTSTP, handler_suspend);
 
+    // initialisation des structures
     bg_processes = malloc(sizeof(ProcessesList));
     bg_processes->nb = 0;
     pthread_mutex_init(&bg_processes->mutex, NULL);
@@ -370,6 +388,9 @@ int main() {
 
         /* If input stream closed, normal termination */
         if (!l) {
+            // libération de la mémoire et arrêt du gestionnaire de sections critiques
+
+            // arrière plan
             pthread_mutex_destroy(&bg_processes->mutex);
 
             for (int i = 0; i < bg_processes->nb; i++) {
@@ -384,6 +405,7 @@ int main() {
             free(bg_processes);
 
 
+            // premier plan
             pthread_mutex_destroy(&fg_processes->mutex);
 
             for (int i = 0; i < fg_processes->nb; i++) {
@@ -407,7 +429,7 @@ int main() {
             continue;
         }
 
-        if (exec_shell_cmd(l) == 0) {
+        if (exec_shell_cmd(l) == 0) { // si ce n'est pas une commande intégrée au shell
             exec_cmd(l);
         }
     }
